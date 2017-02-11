c374deccccbd24a1e78312d29324edef8f06eeef
dimon77 <dimon@Mac-mini-Dmitrij.local>
2017-02-11 18:01:26 +0300

    1801

--- js/min/script-min.js
+++ js/min/script-min.js
@@ -1,1 +1,1 @@
-$(window).scroll(function(){$(this).scrollTop()>30?$("nav").addClass("nav__white"):$("nav").removeClass("nav__white")}),$(".btnright").click(function(a){a.preventDefault(),$(".about").addClass("about--act")});
+$(window).scroll(function(){$(this).scrollTop()>30?$("nav").addClass("nav__white"):$("nav").removeClass("nav__white")}),$(".btnright").click(function(t){t.preventDefault(),$(".about").addClass("about--act")}),$(".select1text2").click(function(t){t.preventDefault(),$(".options").slideToggle("slow")});
--- js/script.js
+++ js/script.js
@@ -9,5 +9,40 @@ $(window).scroll(function () {
 
 $(".btnright").click(function(e) {
     e.preventDefault();
-    $(".about").addClass("about--act")
-})
+    $(".about").addClass("about--act");
+});
+
+$(".select1text2").click(function(e) {
+    e.preventDefault();
+    // $(".options").toggleClass("options--act");
+    $(".options").slideToggle( "slow" );
+});
+
+// var copyTextBtn = document.querySelector('.js-copybtn');
+// copyTextBtn.addEventListener('click', function(event) {
+//   // Выборка ссылки с электронной почтой
+//   var copyText = document.querySelector('.js-copytext');
+//   var range = document.createRange();
+//   range.selectNode(copyText);
+//   window.getSelection().addRange(range);
+//
+//   try {
+//     // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+//     var successful = document.execCommand('copy');
+//     var msg = successful ? 'successful' : 'unsuccessful';
+//     console.log('Copy email command was ' + msg);
+//   } catch(err) {
+//     console.log('Oops, unable to copy');
+//   }
+//
+//   // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+//   // removeRange(range) когда это возможно
+//   window.getSelection().removeAllRanges();
+// });
+
+// $(".js-copybtn").click(function(e) {
+//     e.preventDefault();
+//     var txtCit = $(".js-copytext").html();
+//     console.log(txtCit);
+//     document.execCommand("copy");
+// });
